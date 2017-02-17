8ab8c592ea41b3b1a59dfbfc90acc45ebe14588b
psanker <patricksanker@gmail.com>
2017-02-17 17:23:01 -0500

    Added limit search algorithm
    + variable tolerance linear estimation of exponential limit tails
    + library ZERO definition to avoid dividebyzero exceptions

--- photoelectric/lab.py
+++ photoelectric/lab.py
@@ -42,6 +42,8 @@ m_n  = const.m_n
 m_p  = const.m_p
 R_H  = 2.18e-18 * u.J
 
+ZERO = 0.00001
+
 #############################################################
 # 3. General Functions
 #############################################################
@@ -55,7 +57,11 @@ def lsq(x, y):
     varx = cov[0][0]
     vary = cov[1][1]
     sxy  = cov[0][1]
-    r    = sxy / (np.sqrt(vary) * np.sqrt(varx))
+
+    if np.sqrt(vary) is not np.sqrt(varx):
+        r = sxy / (np.sqrt(vary) * np.sqrt(varx))
+    else:
+        r = np.sign(sxy) * 1
 
     # lambda expression for a line
     # dummy parameter array of [1, 1]
@@ -72,50 +78,6 @@ def lsq(x, y):
     # y = mx + b; r is correlation
     return m, b, sy, sm, sb, r
 
-#############################################################
-# 4. Data
-#############################################################
-
-# 4358 Wavelength
-
-wavelength_4358_V_1 = np.array([0, .68, .78, .85, .95, 1.05, 1.15, 1.25, 1.35, 1.45, 1.55, 1.65, 1.75, 1.85])
-wavelength_4358_d_1 = np.array([-60, 3, 5, 6, 7, 8, 8, 9, 9, 9, 9, 9, 9, 9])
-
-wavelength_4358_V_2 = np.array([0, .5, .6, .7, .8, .9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8])
-wavelength_4358_d_2 = np.array([-61, -5, 0, 3, 5, 7, 8, 8, 8, 9, 9, 9, 9, 9, 9])
-
-  # Appended for complete data set
-wavelength_4358_V = np.append(wavelength_4358_V_1, wavelength_4358_V_2)
-wavelength_4358_d = np.append(wavelength_4358_d_1, wavelength_4358_d_2)
-
-# 546 Wavelength
-
-wavelength_546_V_1 = np.array([0, .2, .3, .4, .5, .6, .7, .8, .9, 1.0, 1.1, 1.2, 1.3, 1.4])
-wavelength_546_d_1 = np.array([-23, -10, -5, -1, 0, 1, 1.5, 2, 2, 2, 2, 2, 2, 2])
-
-wavelength_546_V_2 = np.array([0, .2, .3, .4, .5, .6, .7, .8, .9, 1.0, 1.1, 1.2, 1.3, 1.4])
-wavelength_546_d_2 = np.array([-21, -10, -5, -1, 0, 1, 2, 2, 2, 2, 2, 2, 2, 2])
-
-# Appended for complete data set
-wavelength_546_V = np.append(wavelength_546_V_1, wavelength_546_V_2)
-wavelength_546_d = np.append(wavelength_546_d_1, wavelength_546_d_2)
-
-# 577 Wavelength -- The two runs are identical
-
-wavelength_577_V_1 = np.array([0, .2, .3, .4, .5, .6, .7, .8, .9, 1.0])
-wavelength_577_d_1 = np.array([-5, -2, -1, 0, 0, 0, 0, 0, 0, 0])
-
-wavelength_577_V_2 = np.array([0, .2, .3, .4, .5, .6, .7, .8, .9, 1.0])
-wavelength_577_d_2 = np.array([-5, -2, -1, 0, 0, 0, 0, 0, 0, 0])
-# Appended for complete data set
-wavelength_577_V = np.append(wavelength_577_V_1, wavelength_577_V_2)
-wavelength_577_d = np.append(wavelength_577_d_1, wavelength_577_d_2)
-
-
-#############################################################
-# 5. Lab-specific functions
-#############################################################
-
 def exponential_limit_fit(x, y):
     assert len(x) == len(y), 'Array dimensions do not match'
 
@@ -124,7 +86,11 @@ def exponential_limit_fit(x, y):
     varx = cov[0][0]
     vary = cov[1][1]
     sxy  = cov[0][1]
-    r    = sxy / (np.sqrt(vary) * np.sqrt(varx))
+
+    if np.sqrt(vary) is not np.sqrt(varx):
+        r = sxy / (np.sqrt(vary) * np.sqrt(varx))
+    else:
+        r = np.sign(sxy) * 1
 
     # lambda expression for a line
     # dummy parameter array of [1, 1]
@@ -141,8 +107,86 @@ def exponential_limit_fit(x, y):
 
     return A, sA, B, sB, l, sl, r
 
+#############################################################
+# 4. Data
+#############################################################
+
+# 4358 Wavelength
+
+wavelength_4358_V_1 = np.array([ZERO, .68, .78, .85, .95, 1.05, 1.15, 1.25, 1.35, 1.45, 1.55, 1.65, 1.75, 1.85])
+wavelength_4358_d_1 = np.array([-60, 3, 5, 6, 7, 8, 8, 9, 9, 9, 9, 9, 9, 9])
+
+wavelength_4358_V_2 = np.array([ZERO, .5, .6, .7, .8, .9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8])
+wavelength_4358_d_2 = np.array([-61, -5, ZERO, 3, 5, 7, 8, 8, 8, 9, 9, 9, 9, 9, 9])
+
+  # Appended for complete data set
+wavelength_4358_V = np.append(wavelength_4358_V_1, wavelength_4358_V_2)
+wavelength_4358_d = np.append(wavelength_4358_d_1, wavelength_4358_d_2)
+
+# 546 Wavelength
+
+wavelength_546_V_1 = np.array([ZERO, .2, .3, .4, .5, .6, .7, .8, .9, 1.0, 1.1, 1.2, 1.3, 1.4])
+wavelength_546_d_1 = np.array([-23, -10, -5, -1, ZERO, 1, 1.5, 2, 2, 2, 2, 2, 2, 2])
+
+wavelength_546_V_2 = np.array([ZERO, .2, .3, .4, .5, .6, .7, .8, .9, 1.0, 1.1, 1.2, 1.3, 1.4])
+wavelength_546_d_2 = np.array([-21, -10, -5, -1, ZERO, 1, 2, 2, 2, 2, 2, 2, 2, 2])
+
+# Appended for complete data set
+wavelength_546_V = np.append(wavelength_546_V_1, wavelength_546_V_2)
+wavelength_546_d = np.append(wavelength_546_d_1, wavelength_546_d_2)
+
+# 577 Wavelength -- The two runs are identical
+
+wavelength_577_V_1 = np.array([ZERO, .2, .3, .4, .5, .6, .7, .8, .9, 1.0])
+wavelength_577_d_1 = np.array([-5, -2, -1, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO])
+
+wavelength_577_V_2 = np.array([ZERO, .2, .3, .4, .5, .6, .7, .8, .9, 1.0])
+wavelength_577_d_2 = np.array([-5, -2, -1, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO])
+# Appended for complete data set
+wavelength_577_V = np.append(wavelength_577_V_1, wavelength_577_V_2)
+wavelength_577_d = np.append(wavelength_577_d_1, wavelength_577_d_2)
+
+
+#############################################################
+# 5. Lab-specific functions
+#############################################################
+
+def find_limit_asymptote(x, y, tolerance=0.05):
+    # for data set {x,y}, find a linear approximation of the tail
+    # needs at least 3 data points to function safely
+
+    assert len(x) == len(y), 'Array dimensions do not match'
+
+    m, b, sy, sm, sb, r = (0, 0, 0, 0, 0, 0)
+    run = True
+
+    set_x = x
+    set_y = y
+
+    while (run):
+        if len(set_x) < 3:
+            print('Could not find sufficient asymptote approximation; widen tolerance?')
+            return np.zeros(6)
+
+        # generate lsq line
+        m, b, sy, sm, sb, r = lsq(set_x, set_y)
+
+        # find weighted deviations
+        diffs    = ((m*set_x + b) - set_y)**2 / set_y**2
+        meandiff = np.sum(np.sqrt(diffs)) / float(len(diffs))
+
+        if meandiff > tolerance:
+            set_x = set_x[1:]
+            set_y = set_y[1:]
+        else:
+            run = False
+
+    return np.array([m, b, sy, sm, sb, r])
+
+
 def plot_4358():
-    A, sA, B, sB, l, sl, r = exponential_limit_fit(wavelength_4358_V,wavelength_4358_d)
+    A, sA, B, sB, l, sl, r = exponential_limit_fit(wavelength_4358_V, wavelength_4358_d)
+    lim                    = find_limit_asymptote(wavelength_4358_V, wavelength_4358_d)
 
     x = np.linspace(0, 1.9, 1000)
 
@@ -150,9 +194,12 @@ def plot_4358():
     plt.plot(wavelength_4358_V, wavelength_4358_d, 'r.')
     plt.plot(x, A - B*np.exp(-1*l*x), 'b--', label=('r= %f' % (r)))
 
+    if lim is not np.zeros(6):
+        plt.plot(x, lim[0]*x + lim[1], 'g--', label='Limit')
+
     plt.xlabel('Voltage ($V$)')
     plt.ylabel('Deflection ($mm$)')
-    plt.legend(loc='upper left')
+    plt.legend(loc='lower right')
 
     plt.annotate('$f(x)=A + Be^{-\\lambda x}$\n$A=$%f±%f\n$B=$%f±%f\n$\\lambda=$%f±%f' % (A, sA, B, sB, l, sl), xy=(1, 5), xytext=(1, -20), arrowprops=dict(facecolor='black', headwidth=6, width=.2, shrink=0.05))
 
