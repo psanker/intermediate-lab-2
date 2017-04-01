d635ba6bd1415a9990b9473379af720e6f66e88d
psanker <patricksanker@gmail.com>
2017-03-31 22:53:50 -0400

    yay new lab, new work
    + Added second derivative fetch func

--- bragg/lab.py
+++ bragg/lab.py
@@ -94,60 +94,22 @@ def lsq(x, y):
     # y = mx + b; r is correlation
     return m, b, sy, sm, sb, r
 
+# Calculates the approximate second derivative at an index point with a specified dx
+def second_deriv(arr, i, dx):
+
+    if dx <= 0.:
+        raise Exception('dx must be greater than 0')
+
+    if len(arr) == (i + 1) or i == 0:
+        raise Exception('Cannot perform second derivative at endpoints')
+
+    else:
+        return (arr[i + 1] - 2.*arr[i] + arr[i - 1]) / dx
+
 #############################################################
 # 4. Data
 #############################################################
 
-res     = np.array([16.1, 20.1, 25.1, 30.1, 31.6, 35.6, 40.6, 45.6, 50.6, 55.6, 60.6, 65.6, 70.6, 74.6, 75.0, 80.0, 85.0, 90.0, 95.0, 100.0])
-current = np.array([.347, .438, .546, .658, .694, .781, .891, 1.0, 1.109, 1.219, 1.328, 1.436, 1.550, 1.632, 1.612, 1.716, 1.831, 1.933, 2.043, 2.152])
-Bfield  = 2.115 * current
-
-dW      = 1.8
-I_modpp = 2*np.sqrt(2)*0.493
-
 #############################################################
 # 5. Lab-specific functions
 #############################################################
-
-def plot_line():
-    '''
-    Plots a linear fit for the Magnetic Field and Resonance Frequency Data
-    '''
-    m, b, sy, sm, sb, r = lsq(Bfield, res)
-
-    x = np.linspace(Bfield[0], Bfield[-1], 1000)
-
-    plt.figure()
-
-    plt.plot(x, m*x + b, 'b--', label='Linear Fit')
-    plt.errorbar(Bfield, res, xerr=2.115*.001, fmt='r.', ecolor='k', label='Magnetic Field Data')
-
-    plt.annotate('$y=mx + b$\n$m=$%.3f$\pm$%.3f\n$b=$%.3f$\pm$%.3f\n$r=$%.4f' % (m, sm, b, sb, r), xy=(2.6, 54), xytext=(3.1, 27.4), arrowprops=dict(facecolor='black', headwidth=6, width=.2, shrink=0.05))
-    plt.xlabel('Magnetic Field ($mT$)')
-    plt.ylabel('Resonant Frequency ($MHz$)')
-    plt.legend(loc='upper left')
-
-
-def get_gee():
-    '''
-    Calculates the value of g, the ratio of the electron’s magnetic moment in units of the Bohr magneton to its angular momentum in units of hbar, using the slope of the linear fit
-    '''
-    m, b, sy, sm, sb, r = lsq(Bfield, res)
-
-    muB   = (q_e.value*hbar.value) / (2.*m_e.value)
-    slope = m * 1e9 #for unit conversion
-
-    gee  = (h.value * slope) / muB
-    dgee = (sm / m) * gee
-
-    return gee, dgee
-
-
-def get_deltaB():
-    '''
-    Calculates delta B, the full width at half height of the resonance in terms of the magnetic field.
-    '''
-    deltaB  = ((I_modpp*dW) / 10.) * 2.115
-    sdeltaB = np.sqrt((.2/1.8)**2 + (.001/.493)**2)*deltaB
-
-    return deltaB, sdeltaB
