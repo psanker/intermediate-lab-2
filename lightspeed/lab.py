f8d06fa102267666d4d7afeacb2936162a8d9291
kjm538 <kjm538@nyu.edu>
2017-04-08 12:35:37 -0400

    our light was a little slow

--- lightspeed/lab.py
+++ lightspeed/lab.py
@@ -99,10 +99,28 @@ mirror_dist = .05 #meters
 xtra = source_dist - mirror_dist
 temp = 20.0 #celsius
 
-air_x = np.array([101.0, 102.0, 103.0, 104.0, 105.0, 106.0, 107.0, 108.0, 109.0, 110.0]) * 1e-2 + xtra
-air_t = np.array([0.0, 36.0, 96.0, 176.0, 236.0, 316.0, 400.0, 436.0, 536.0, 616.0]) * 1e-9 #seconds
+air_x = np.array([101.0, 102.0, 103.0, 104.0, 105.0, 106.0, 107.0, 108.0, 109.0, 110.0]) * 2 * 1e-2 + xtra
+air_t = np.array([0.0, 36.0, 96.0, 176.0, 236.0, 316.0, 400.0, 436.0, 536.0, 616.0]) * 1e-12 #seconds
 xi = np.ones([4])*56.5 * 1e-2 + xtra
 water_xf = np.array([89.0, 87.3, 86.4, 86.8]) * 1e-2 + xtra
 poly_xf = np.array([78.0, 77.9, 79.0, 77.9]) * 1e-2 + xtra
 glass_xf = np.array([84.0, 83.6, 86.7, 85.3]) * 1e-2 + xtra
 unknown_xf = np.array([106.3, 109.9, 107.7, 107.8]) * 1e-2 + xtra
+disterr = np.sqrt((.002/(air_x - xtra))**2 + (.002/.095)**2 + (.01/.5)**2) * air_x
+timeerr = 5.7 * 1e-12
+
+#############################################################
+# 5. Lab-specific functions
+#############################################################
+
+def plot_airspeed():
+    x = np.linspace(0, 616*1e-12, 1000)
+
+    m, b, sy, sm, sb, r = lsq(air_t, air_x)
+    plt.figure()
+    plt.plot(air_t, air_x, 'r.', label='Air Data')
+    plt.plot(x, m*x+b, 'b-', label='Linear Fit')
+    plt.annotate('$y=mx + b$\n$m=$%1.3e$\pm$%1.3e\n$b=$%1.3e$\pm$%1.3e\n$r=$%1.3f' % (m, sm, b, sb, r), xy=(2.9e-10, 2.15), xytext=(3.1e-10, 2.08), arrowprops=dict(facecolor='black', headwidth=6, width=.2, shrink=0.05))
+    plt.xlabel('Time Delay (s)')
+    plt.ylabel('Path Length (m)')
+    plt.legend(loc='upper left')
